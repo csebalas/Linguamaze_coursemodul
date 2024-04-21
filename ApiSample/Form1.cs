368295d9dc82d257d10967da04c2ef2e1c86ce7b
Balázs Cseke <csebalas2003@gmail.com>
2024-04-21 16:09:23 +0200

    Hibás api kérés javítva

--- ApiSample/Form1.cs
+++ ApiSample/Form1.cs
@@ -26,14 +26,6 @@ namespace ApiSample
             var url = string.Empty;
             var key = string.Empty;
 
-            // Kezdőértékek beállítása vagy parancssori argumentumokból olvasás
-            string[] args = Environment.GetCommandLineArgs();
-            if (args.Length > 2)
-            {
-                url = args[1];
-                key = args[2];
-            }
-
             if (url == string.Empty) url = "http://20.234.113.211:8101/";
             if (key == string.Empty) key = "1-64869949-9801-4b5c-bd4b-326377c14130";
 
@@ -43,64 +35,37 @@ namespace ApiSample
             var page = 1;
             var pageSize = int.MaxValue;
 
-            
-
             Api proxy = new Api(url, key);
 
-            var snaps = proxy.CategoriesFindAll();
-            var product = proxy.ProductsFindAll();
+            //var snaps = proxy.CategoriesFindAll();
+            //var product = proxy.ProductsFindAll();
             ApiResponse<PageOfProducts> response = proxy.ProductsFindForCategory(categoryId, page, pageSize);
-            Console.WriteLine(response.Content.ToString());
-            if (snaps.Content != null)
+            if (response.Errors.Count > 0)
             {
-                Console.WriteLine(response.Content.ToString());
-                //Console.WriteLine("Found " + snaps.Content.Count + " categories");
-                //Console.WriteLine("-- First 5 --");
-                for (var i = 0; i < 5; i++)
+                foreach (var err in response.Errors)
                 {
-                    //if (i < snaps.Content.Count)
-                    //{
-                       // Console.WriteLine(i + ") " + snaps.Content[i].Name + " [" + snaps.Content[i].Bvin + "]");
-                        list.Add(snaps.Content[i].Name);
-                        var cat = proxy.CategoriesFind(snaps.Content[i].Bvin);
-                        if (cat.Errors.Count > 0)
-                        {
-                            foreach (var err in cat.Errors)
-
-                            {
-                                //Console.WriteLine("ERROR: " + err.Code + " " + err.Description);
-                            }
-                        }
-                        else
-                        {
-                            //Console.WriteLine("By Bvin: " + cat.Content.Name + " | " + cat.Content.Description);
-                        }
-
-                        var catSlug = proxy.CategoriesFindBySlug(snaps.Content[i].RewriteUrl);
-                        if (catSlug.Errors.Count > 0)
-                        {
-                            foreach (var err in catSlug.Errors)
-                            {
-                                //Console.WriteLine("ERROR: " + err.Code + " " + err.Description);
-                            }
-                        }
-                        else
-                        {
-                            //Console.WriteLine("By Slug: " + cat.Content.Name + " | " + cat.Content.Description);
-                        }
-                    //}
+                    Console.WriteLine("ERROR: " + err.Code + " " + err.Description);
                 }
             }
+            else if (response.Content == null)
+            {
+                Console.WriteLine("No content returned from the API call.");
+            }
+            else
+            {
+                List<string> productNames = new List<string>();
+                foreach (var product in response.Content.Products)
+                {
+                    Console.WriteLine(product.ProductName);
+                    productNames.Add(product.ProductName);
+                }
+                listBoxKurzusok.DataSource = productNames;
+            }
 
-            //Console.WriteLine("Done - Press a key to continue");
-            //Console.ReadKey();
-
-            listBoxKurzusok.DataSource = list;
-        }
-
-        void TeltBetoltes()
-        {
-            //var eredmen
+            void TeltBetoltes()
+            {
+                //var eredmen
+            }
         }
     }
 }
